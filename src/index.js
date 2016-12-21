d277bad04cc62f7a40f77f28bdb13e6c475e20bd
Jim Tian <yytian@uwaterloo.ca>
2016-12-20 23:06:01 -0500

    Migrate to webpack 2. Treeshaking works, hot-reload doesn't yet (wait for inotify)

--- src/index.js
+++ src/index.js
@@ -7,15 +7,26 @@ import Game from './containers/Game';
 import reducer from './reducers';
 import type { Store } from './types';
 
+import { AppContainer } from 'react-hot-loader';
+
 require('./assets/board.css');
 require('./assets/spritesheet-cn.css');
 
 const store: Store = createStore(reducer);
 const state = store.getState();
 
-render(
-    <Provider store={store}>
-        <Game />
-    </Provider>,
-    document.getElementById('root'),
-);
+const run = () => {
+    render(
+        <Provider store={store}>
+            <Game />
+        </Provider>,
+	document.getElementById('root'),
+    );
+};
+
+run();
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./containers/Game', run);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,68 @@ try {
   require('os').networkInterfaces = () => ({});
 }
 
+const { resolve } = require('path');
+const webpack = require('webpack');
+
 module.exports = {
-     entry: './src/index.js',
-     output: {
-         path: './bin',
-         filename: 'app.bundle.js'
-     },
-     module: {
-         loaders: [
-             {
-                 test: /\.js$/,
-                 exclude: /node_modules/,
-                 loader: 'babel-loader'
-             },
-             {
-                 test: /\.css$/,
-                 exclude: /node_modules/,
-                 loader: 'style-loader!css-loader'
-             },
-             { test: /\.png$/, loader: "file-loader" }
-         ]
-     }
- };
+    entry: [
+	'react-hot-loader/patch',
+	// activate HMR for React
+	'webpack-dev-server/client?http://localhost:8080',
+	// bundle the client for webpack-dev-server
+	// and connect to the provided endpoint
+	'webpack/hot/only-dev-server',
+	// bundle the client for hot reloading
+	// only- means to only hot reload for successful updates
+	'./index.js'
+	// the entry point of our app
+    ],
+    output: {
+	filename: 'app.bundle.js',
+	// the output bundle
+	path: resolve(__dirname, 'bin'),
+	publicPath: '/'
+	// necessary for HMR to know where to load the hot update chunks
+    },
+    context: resolve(__dirname, 'src'),
+    devtool: 'inline-source-map',
+    devServer: {
+	hot: true,
+	// activate hot reloading
+	contentBase: resolve(__dirname, 'bin'),
+	// match the output path
+	publicPath: '/'
+	// match the output `publicPath`
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loaders: ['style-loader', 'css-loader']
+            },
+            { test: /\.png$/, loader: "file-loader", exclude: /node_modules/ }
+        ]
+    },
+    plugins: [
+	new webpack.HotModuleReplacementPlugin(),
+	// activates HMR
+	new webpack.NamedModulesPlugin(),
+	// prints more readable module names in the browser console on HMR updates
+	new webpack.DefinePlugin({
+	    'process.env':{
+		'NODE_ENV': JSON.stringify('production')
+	    }
+	}),
+	new webpack.optimize.UglifyJsPlugin({
+	    compress:{
+		warnings: false
+	    }
+	})
+    ],
+};
